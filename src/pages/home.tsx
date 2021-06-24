f2b871d9608daa1a3f3653dd565a30f8e27b5c83
SenhorBiscoito <mirandajean2009@gmail.com>
2021-06-23 23:18:05 -0300

    Criando o hook de autenticacao

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,3 @@
-import { auth, firebase } from "../services/firebase";
 import { useHistory } from "react-router-dom";
 
 import illustrationImg from "../assets/images/illustration.svg";
@@ -7,12 +6,11 @@ import googleIconImg from "../assets/images/google-icon.svg";
 
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
-import { AuthContext } from "../App";
-import { useContext } from "react";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
